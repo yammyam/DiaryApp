25635c6b315826d553e04669524d1135b8b44f84
yammyam <tkdxo999@gmail.com>
2023-04-23 23:54:55 +0900

    일기장 CRUD

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,6 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onDelete, dummyData }) => {
+const DiaryList = ({ onEdit, onRemove, dummyData }) => {
   console.log(dummyData);
   return (
     <div className="DiaryList">
@@ -8,7 +8,7 @@ const DiaryList = ({ onDelete, dummyData }) => {
       <h4>{dummyData.length}개의 내용이 있습니다</h4>
       <div>
         {dummyData.map((el) => (
-          <DiaryItem key={el.id} {...el} onDelete={onDelete} /> //두개 건너서 가니까 이런걸 프롭스 드릴링이라고 함.
+          <DiaryItem onEdit={onEdit} key={el.id} {...el} onRemove={onRemove} /> //두개 건너서 가니까 이런걸 프롭스 드릴링이라고 함.
         ))}
       </div>
     </div>
